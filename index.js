c0b60cf3734898a2bd4bcaaa955099978814c0bb
Joe Fearnley <joe.fearnley@gmail.com>
2018-10-25 17:54:06 +0000

    Adding scraping in main file

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const cheerio = require('cheerio');
+const curl = new (require( 'curl-request' ))();
+
+curl.get('https://www.musiciansfriend.com/stupid')
+.then(({statusCode, body, headers}) => {
+  const $ = cheerio.load(body);
+
+  const product = $('.feature-title h2 a').text();
+  const description = $('.feature-description').text();
+  const regularPrice = $('.regular-price').text();
+  const savings = $('.feature-save .formatted-price').text();
+  const price = $('.feature-price .formatted-price').text();
+
+  console.log(product);
+  console.log(description);
+  console.log(regularPrice);
+  console.log(savings);
+  console.log(price);
+
+  //const postBody = ``;
+
+})
+.catch((e) => {
+    console.log(e);
+});
+
