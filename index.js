7fe533b5e49691180007a40ffbb788be7dd984b6
Joe Fearnley <joe.fearnley@gmail.com>
2018-11-07 21:05:36 +0000

    Checking to see if there is actually a deal of the day

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-
 const cheerio = require('cheerio');
 const curl = new (require( 'curl-request' ))();
 
 curl.get('https://www.musiciansfriend.com/stupid')
-.then(({statusCode, body, headers}) => {
-  const $ = cheerio.load(body);
+  .then(({statusCode, body, headers}) => {
+    const $ = cheerio.load(body);
 
-  const product = $('.feature-title h2 a').text();
-  const description = $('.feature-description').text();
-  const regularPrice = $('.regular-price').text();
-  const savings = $('.feature-save .formatted-price').text();
-  const price = $('.feature-price .formatted-price').text();
+    const product = $('.feature-title h2 a').text();
+    const description = $('.feature-description').text();
+    const regularPrice = $('.regular-price').text();
+    const savings = $('.feature-save .formatted-price').text();
+    const price = $('.feature-price .formatted-price').text();
 
-  console.log(product);
-  console.log(description);
-  console.log(regularPrice);
-  console.log(savings);
-  console.log(price);
+    if (!product) {
+      return;
+    }
 
-  const postBody = `
-  # ${product}
-  
-  ${description}
-  
-  ${regularPrice}
-  ${savings}
-
-  **${price}**
-  `;
-
-})
-.catch((e) => {
-    console.log(e);
-});
-
+    const postBody = `
+      # ${product}\n${description}\n\nRegular Price: ${regularPrice}\nSavings: ${savings}\n\n**${price}**\n\n[http://musiciansfriend.com/stupid](http://musiciansfriend.com/stupid)
+    `;
+    
+    console.log(postBody);
+  }).catch(e => console.log(e));
